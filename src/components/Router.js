8d88cccda888ee91e391d0005d03d5b2318934d5
HweDongJung <asky1193@gmail.com>
2023-08-03 16:14:08 +0900

    router basical setting

---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+//유저의 상태, 동작에 따른 페이지 라우팅을 담당하는 코드 
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); //로그인 상태인가에 대한 state, 기본 false, useState Hook으로 상태 초기화 
+    return (
+        <Router>
+            <Routes>
+                {isLoggedIn ? 
+                <>
+                    <Route exact path="/" element={<Home />}></Route> 
+                </> 
+                : <Route exact path="/" element={<Auth />}></Route>
+                } 
+            </Routes> 
+
+        </Router>
+    ) 
+}
+
+export default AppRouter;
